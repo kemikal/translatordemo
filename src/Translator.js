e521ee58a406da421ad4085030f1ef52d0393868
Janne Kemi <jannekemi@MacBook-Pro-som-tillhor-Janne.local>
2021-08-29 10:20:05 +0200

    Fix API link

--- src/Translator.js
+++ src/Translator.js
@@ -1,6 +1,6 @@
 function Translator(cb, textToTranslate, lang) {
 
-   fetch("https://api.mymemory.translated.net/get?d=janne@hiveandfive.se&q=" + textToTranslate + "!&langpair=sv|" + lang)
+   fetch("https://api.mymemory.translated.net/get?&q=" + textToTranslate + "!&langpair=sv|" + lang)
    .then(response => response.json())
    .then(data => {
        //console.log(data.responseData.translatedText);
